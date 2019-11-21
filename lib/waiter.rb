80b06f5ba38d56e7bc588fa10cc28d69856caca5
Taylor Kraft <taylor.kraft19@gmail.com>
2019-11-21 19:47:04 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -16,4 +16,18 @@ class Waiter
   def new_meal(customer, total, tip = 0)
     Meal.new(self, customer, total, tip)
   end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+  
+  def best_tipper
+    best_tipped_meal = meals.max
+    do |meal_a.tip <=> meal_b.tip
+  end
+  
+    best_tipped_meal.customer
+  end
 end
